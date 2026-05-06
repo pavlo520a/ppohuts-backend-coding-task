1fbdd3358c6c3eff690794dc602313d87e4a53f8
pavlo520a <pavlo520a@gmail.com>
2026-05-06 17:35:34 +0300

    fix: bug fix

--- Claims.Data/DependencyInjection.cs
+++ Claims.Data/DependencyInjection.cs
@@ -13,10 +13,16 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddData(this IServiceCollection services)
     {
+        services.AddSingleton(serviceProvider =>
+        {
+            var mongoOptions = serviceProvider.GetRequiredService<IOptions<MongoDbOptions>>().Value;
+            return new MongoClient(mongoOptions.ConnectionString);
+        });
+
         services.AddDbContext<ClaimsMongoDbContext>((serviceProvider, options) =>
         {
             var mongoOptions = serviceProvider.GetRequiredService<IOptions<MongoDbOptions>>().Value;
-            var client = new MongoClient(mongoOptions.ConnectionString);
+            var client = serviceProvider.GetRequiredService<MongoClient>();
             var database = client.GetDatabase(mongoOptions.DatabaseName);
             options.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName);
         });
