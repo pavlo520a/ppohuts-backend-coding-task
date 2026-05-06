0e76158313e64c7f1918e9dc64d1b1c1832566ca
pavlo520a <pavlo520a@gmail.com>
2026-05-06 23:47:02 +0300

    fix: fix test's build

--- Claims.Application.UnitTests/UseCases/OutboxUseCasesTests.cs
+++ Claims.Application.UnitTests/UseCases/OutboxUseCasesTests.cs
@@ -122,7 +122,7 @@ public sealed class OutboxUseCasesTests
     {
         public int AddCalls { get; private set; }
 
-        public void Add<TAuditOutbox>(TAuditOutbox auditOutbox) where TAuditOutbox : BaseAuditOutbox
+        public void Add(BaseAuditOutbox auditOutbox)
         {
             AddCalls++;
         }
