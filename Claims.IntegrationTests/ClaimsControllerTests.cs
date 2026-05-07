6f6676a7979cb46c1c27413a25891b8126453fdf
pavlo520a <pavlo520a@gmail.com>
2026-05-07 15:10:57 +0300

    refactor: rename project to Claims.IntegrationTests

--- Claims.IntegrationTests/ClaimsControllerTests.cs
+++ Claims.IntegrationTests/ClaimsControllerTests.cs
@@ -1,4 +1,4 @@
-﻿using Claims.Application.Abstractions;
+using Claims.Application.Abstractions;
 using Claims.Application.Commands.Claims;
 using Claims.Domain.Enums;
 using Claims.Domain.Models;
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 
-namespace Claims.Tests
+namespace Claims.IntegrationTests
 {
     public class ClaimsControllerTests
     {
